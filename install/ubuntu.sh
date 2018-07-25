848437157a9fc5f5a212f31a2ee0c2f653c2c83d
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2018-07-25 16:58:08 +0900

    added docker command

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -8,18 +8,17 @@ command_exists() {
 env LANGUAGE=C LC_MESSAGES=C xdg-user-dirs-gtk-update
 
 # Upgrade
-printf "password: "
-read password
-echo "$password" | sudo -S apt -y upgrade && apt update && apt autoremove && apt clean
+read -sp "Password: " password
+echo $password | sudo -S apt -y upgrade && apt update && apt autoremove && apt clean
 
 # Install if zsh is not installed
 if ! command_exists zsh; then
-  echo "$password" | sudo -S apt install zsh
+  echo $password | sudo -S apt install zsh
 fi
 
 # Install if tmux is not installed
 if ! command_exists tmux; then
-  echo "$password" | sudo -S apt install tmux
+  echo $password | sudo -S apt install tmux
 fi
 
 # Change the default shell to zsh
@@ -35,4 +34,24 @@ if [[ "$SHELL" != "$zsh_path" ]]; then
 fi
 
 # install basic package
-echo "$password" | sudo -S apt install tree curl vim vim-gnome code
+echo $password | sudo -S apt install -y tree curl vim vim-gnome code apt-transport-https ca-certificates software-properties-common
+
+if ! command_exists docker; then
+  # gpd pub key install
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+  # check public key
+  sudo apt-key fingerprint 0EBFCD88
+
+  # add repo
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+  # update 
+  sudo apt-get update
+
+  # install docker
+  sudo apt-get install -y docker-ce
+fi
