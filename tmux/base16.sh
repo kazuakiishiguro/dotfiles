fb33215500579c2ce5e0ec51b20de5bc4aa083d9
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2018-09-06 19:16:40 +0900

    fixed color

--- tmux/base16.sh
+++ tmux/base16.sh
@@ -1,54 +1,34 @@
-# Base16 Styling Guidelines:
-# base00 - Default Background
-# base01 - Lighter Background (Used for status bars)
-# base02 - Selection Background
-# base03 - Comments, Invisibles, Line Highlighting
-# base04 - Dark Foreground (Used for status bars)
-# base05 - Default Foreground, Caret, Delimiters, Operators
-# base06 - Light Foreground (Not often used)
-# base07 - Light Background (Not often used)
-# base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-# base09 - Integers, Boolean, Constants, XML Attributes, Markup Link Url
-# base0A - Classes, Markup Bold, Search Text Background
-# base0B - Strings, Inherited Class, Markup Code, Diff Inserted
-# base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
-# base0D - Functions, Methods, Attribute IDs, Headings
-# base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
-
-base00=default  # #000000
-base01=colour18 # #282828
-base02=colour19 # #383838
-base03=colour8  # #585858
-base04=colour20 # #B8B8B8
-base05=colour7  # #D8D8D8
-base06=colour21 # #E8E8E8
-base07=colour15 # #F8F8F8
-base08=colour01 # #AB4642
-base09=colour16 # #DC9656
-base0A=colour3  # #F7CA88
-base0B=colour2  # #A1B56C
-base0C=colour6  # #86C1B9
-base0D=colour4  # #7CAFC2
-base0E=colour5  # #BA8BAF
-base0F=colour17 # #A16946
+base00=default  
+base01=colour238
+base02=colour255 
+base03=colour8  
+base04=colour20 
+base05=colour7  
+base06=colour21 
+base07=colour15 
+base08=colour01 
+base09=colour16 
+base0A=colour3  
+base0B=colour2  
+base0C=colour6  
+base0D=colour4  
+base0E=colour241
+base0F=colour17 
 
 set -g status-left-length 32
 set -g status-right-length 150
-set -g status-interval 5
+set -g status-interval 1
 
 # -- default statusbar colors -------------------------------------------------
 set-option -g status-fg $base02
 set-option -g status-bg $base01
 set-option -g status-attr default
 
-set-window-option -g window-status-fg $base04
-set-window-option -g window-status-bg $base00
-set -g window-status-format " #I #W"
+set-window-option -g window-status-format "#[fg=$base02] #I: #W"
+
 
 # -- active window title colors -----------------------------------------------
-set-window-option -g window-status-current-fg $base01
-set-window-option -g window-status-current-bg $base0C
-set-window-option -g  window-status-current-format " #[bold]#W "
+set-window-option -g window-status-current-format "#[fg=$base02,bg=$base0C] #I: #W #[default]"
 
 # -- pane border colors -------------------------------------------------------
 set-window-option -g pane-border-fg $base03
@@ -65,13 +45,7 @@ set-option -g display-panes-colour $base01
 # -- clock --------------------------------------------------------------------
 set-window-option -g clock-mode-colour $base0C
 
-tm_session_name="#[default,bg=$base0E,fg=$base01] #S "
-set -g status-left "$tm_session_name"
 
-# tm_tunes="#[bg=$base0D,fg=$base01] ♫ #(osascript -l JavaScript ~/.dotfiles/applescripts/tunes.js)"
-# tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/tunes.scpt | cut -c 1-50)"
-# tm_battery="#(~/.dotfiles/bin/battery_indicator.sh)"
-tm_battery="#[fg=$base01,bg=$base09] ♥ #(battery)"
-tm_date="#[default,bg=$base02,fg=$base05] %R"
-tm_host="#[fg=$base01,bg=$base0E] #h "
-set -g status-right "$tm_tunes $tm_battery $tm_date $tm_host"
+# -- status bar ---------------------------------------------------------------
+set-option -g status-left "#[fg=$base02,bg=$base0E]Session: #S #[default]"
+set-option -g status-right "#[fg=$base02,bg=$base0E] #h | ♥ #(battery) | %m/%d %H:%M:%S#[default]"
