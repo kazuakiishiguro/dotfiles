9bde17a8b2c5fe650d3a74e3bdc25088ab254955
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2018-09-30 22:39:43 +0900

    fix typo

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -36,4 +36,4 @@ alias canary="/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\
 alias pcat='pygmentize -f terminal256 -O style=native -g'
 
 # -- Update setting -----------------------------------------------------------
-alias update!="cd && ${updateCmd} && cd - > null"
+alias update!="cd && ${updateCmd} && cd - > /dev/null"
