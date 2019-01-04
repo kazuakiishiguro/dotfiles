a9b20a3ec498f3db48187a59641fa38ebf275e70
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-01-04 15:47:26 +0900

    🐗

--- install/osx.sh
+++ install/osx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! "$(type brew)" ]; then 
+if [ ! "$(type brew)" ]; then
   echo "Installing homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
