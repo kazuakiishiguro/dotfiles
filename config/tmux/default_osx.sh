35aeaf0cce36ffbbebcb4239bc1876df41364760
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-03-18 12:03:20 +0900

    changed tmux setting

--- config/tmux/default_osx.sh
+++ config/tmux/default_osx.sh
@@ -23,8 +23,8 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"battery 5 231" \
-		#"hostname 36 59" \
+		# "battery 5 231" \
+		"hostname 36 231" \
 		"lan_ip 30 231" \
 		"wan_ip 24 231" \
 	)
@@ -36,8 +36,8 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		"date_day 30 231" \
 		"date 30 231 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		"time 30 231 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		#"weather 36 231" \
-		#"ifstat_sys 36 59" \
+		# "utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		# "weather 36 231" \
+		"ifstat_sys 36 231" \
 	)
 fi
