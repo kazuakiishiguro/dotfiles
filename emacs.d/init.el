a605f1e6c7c7dd2b86d1729367b72ff7535fdbd4
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-05-29 02:11:30 +0900

    added company for emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,41 @@
-;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;; init.el --- Load the full configuration -*- lexical-binding: t -*-
 ;;; Code: 
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;; setting up and install straight.el 
+(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+      (bootstrap-version 3))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(require 'init-lisp)
+;; install use-package
+(straight-use-package 'use-package)
+
+;; fallback without options
+(setq straight-use-package-by-default t)
+
+;; load init-loader
+(use-package init-loader)
+
+;; load files under ~/.emacs.d/lisp/ 
+(init-loader-load "~/.emacs.d/lisp")
+
+(use-package company
+    :init
+    (setq company-selection-wrap-around t)
+    :bind
+    (:map company-active-map
+        ("M-n" . nil)
+        ("M-p" . nil)
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("C-h" . nil))
+    :config
+    (global-company-mode))
 
 ;;; init.el ends here
