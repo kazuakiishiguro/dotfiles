f99aeed9ab46f4ccf0291df2cb545cf264570abf
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-05-28 17:10:35 +0900

    🐗 for emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,8 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; Code: 
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-lisp)
+
+;;; init.el ends here
