3209010e2451e8996d4777b2927f952c56dac547
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-06-11 08:02:28 -0700

    removed Ricty font

--- install.sh
+++ install.sh
@@ -2,16 +2,5 @@
 
 set -eu
 
-echo "installing ricty font for emacs"
-
-if [[ $"fc-list Ricty" ]]; then
-    echo "already installed"
-else
-    brew tap sanemat/font
-    brew install ricty
-    cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-    fc-cache -vf
-fi
-
 rcup -d . -x README.md
 unlink ~/.install.sh
