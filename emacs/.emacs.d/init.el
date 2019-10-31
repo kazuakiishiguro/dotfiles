c9aef601418367f07ce61b75d24f1f9014b723a5
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-10-31 09:07:43 +0900

    fixed title

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,17 +21,4 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org-bullets rust-mode magit flycheck company diff-hl which-key helm dracula-theme use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
