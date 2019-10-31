59a2f0163872bfaf344b5ac66232ddfc24baee3c
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-10-31 09:49:37 +0900

    removed files should not be included

--- emacs/.emacs.d/elpa/rust-mode-20191023.918/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20191023.918" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "484d6754c51424e3b49461d54cfeac8fc05993b5" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
