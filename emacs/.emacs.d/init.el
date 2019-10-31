6a5427feaf71868ce829b777cef5a313e500b660
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-10-31 11:17:17 +0900

    added org-rust

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,3 +21,17 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ob-rust which-key use-package toml-mode racer org-bullets magit helm flycheck dracula-theme diff-hl company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
