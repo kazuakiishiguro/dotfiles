94e672eb384e45cb07d2a76ef977fa0c83844ee7
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2020-04-16 11:06:58 +0900

    remove magit from emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,21 +22,3 @@
 
 ;;; init.el ends here
 
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(dired-hide-details-hide-information-lines nil)
- '(dired-hide-details-hide-symlink-targets nil)
- '(package-selected-packages
-   (quote
-    (tide rjsx-mode vterm which-key web-mode use-package toml-mode racer package-utils org-bullets ob-rust minions magit js2-mode htmlize helm git-gutter flycheck-rust dracula-theme doom-themes company-solidity cargo))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
