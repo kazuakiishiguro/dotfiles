23c6299a4815f7f6d9fde264bffa77774b407232
Kazuaki Ishiguro <gurokazu@gmai.com>
2020-08-23 17:23:22 +0900

    WIP Ported each configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,3 +21,17 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-preview-mode typescript-mode rjsx-mode web-mode solidity-mode ob-rust cargo rust-mode lsp-treemacs company-lsp lsp-ui lsp-mode flycheck company git-gutter fringe-helper minions hlinum ox-gfm htmlize docker-compose-mode dockerfile-mode which-key helm use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
