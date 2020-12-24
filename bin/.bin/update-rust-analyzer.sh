0a3fb83c69f5288b4f89ca2af50f55a55a21b023
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2020-12-24 13:37:04 +0900

    Added rust-analyzer update file

---
+++ bin/.bin/update-rust-analyzer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+echo "Installing rust-analyzer binary \n"
+
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/download/2020-12-21/rust-analyzer-mac -o $HOME/.bin/rust-analyzer
