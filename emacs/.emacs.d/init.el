2df9eddc509a8b199cba51add2a17a76f40dc487
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2022-01-12 22:02:14 +0900

    emacs region color

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,3 +23,17 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rustic ws-butler web-mode use-package tide solidity-mode smartparens rust-mode rjsx-mode python-mode plan9-theme ox-gfm ob-rust multiple-cursors minions markdown-preview-mode magit lsp-treemacs htmlize google-c-style git-gutter fringe-helper dockerfile-mode docker-compose-mode company cmake-mode cargo)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number-current-line ((t (:foreground "Royalblue" :background "DarkGray"))))
+ '(sp-pair-overlay-face ((t nil))))
