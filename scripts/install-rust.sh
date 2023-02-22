e06648aed40ade9437a4825f1855f98b7094d916
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2023-02-23 08:34:29 +0900

    Add Brewfile

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -26,4 +26,6 @@ else
     git clone https://github.com/rust-analyzer/rust-analyzer.git
     cd rust-analyzer
     cargo xtask install --server
+    cd -
+    rm -rf rust-analyzer
 fi
--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -12,20 +12,14 @@ is_command() {
     command -v "${check_command}" > /dev/null 2>&1
 }
 
+cd $(dirname $0)
+
 if is_command brew; then
     echo 'Homebrew is_command. Skipping install.'
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo 'Updating Homebrew...'
 brew update
-
-if is_command ansible; then
-    echo 'Ansible exists. Skipping brew install ansible.'
-else
-    brew install ansible
-fi
-
-# echo 'Ansible playbook exec. Running command ansible-playbook to localhost.'
-ansible-playbook -i localhost, -c local ./scripts/macos.yml
+brew upgrade
+brew bundle --file ./scripts/Brewfile
