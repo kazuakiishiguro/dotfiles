39de9e682a6d5b8bc83e713e7e3db174a3a49db6
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2023-05-02 13:46:13 +0900

    Update ubuntu setup

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -14,28 +14,17 @@ is_command() {
 
 # install fzf
 if is_command fzf; then
-  echo "fzf exists"
+    echo "fzf exists"
 else
-  sudo apt install -y fzf
+    sudo apt install -y fzf
 fi
 
 # install emacs
 if is_command emacs; then
-  echo "emacs exists"
+    echo "emacs exists"
 else
-  sudo apt remove --autoremove emacs emacs-common
-  sudo add-apt-repository ppa:kelleyk/emacs
-  sudo apt update
-  sudo apt-get install -y emacs28
-fi
-
-if is_command Xorg; then # check if desktop or server
-    # install keybase
-    if is_command keybase; then
-	echo "keybase exists"
-    else
-	curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
-	sudo apt install -y ./keybase_amd64.deb
-	run_keybase
-    fi
+    sudo apt remove --autoremove emacs emacs-common
+    sudo add-apt-repository ppa:kelleyk/emacs
+    sudo apt update
+    sudo apt-get install -y emacs28
 fi
