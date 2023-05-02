69a9d71494d842ae792d7e139a15c57c5ccafda9
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2023-05-02 16:13:10 +0900

    Update scripts

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -2,13 +2,7 @@
 
 set -eu
 
-is_command() {
-    # Checks for existence of string passed in as only function argument.
-    # Exit value of 0 when exists, 1 if not exists. Value is the result
-    # of the `command` shell built-in call.
-    local check_command="$1"
-    command -v "${check_command}" > /dev/null 2>&1
-}
+source ./scripts/utils.sh
 
 # check if rust is installed
 if is_command cargo; then
--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -4,13 +4,7 @@ set -eu
 
 arch=`uname -m`
 
-is_command() {
-    # Checks for existence of string passed in as only function argument.
-    # Exit value of 0 when exists, 1 if not exists. Value is the result
-    # of the `command` shell built-in call.
-    local check_command="$1"
-    command -v "${check_command}" > /dev/null 2>&1
-}
+source ./scripts/utils.sh
 
 cd $(dirname $0)
 
