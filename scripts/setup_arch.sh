77f6f24c719b386e346cd007cceb6c21d1f389ef
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2023-08-11 16:15:28 +0900

    Add yay setup

--- scripts/setup_arch.sh
+++ scripts/setup_arch.sh
@@ -2,9 +2,18 @@
 
 set -eu
 
-yay
-
 source ./scripts/utils.sh
+
+if is_command yay; then
+    echo "yay exists"
+    yay
+else
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git ~/fun/yay
+    cd ~/fun/yay && makepkg -si
+fi
+
+yay
 
 # install curl
 if is_command curl; then
@@ -17,5 +26,11 @@ fi
 if is_command emacs; then
     echo "emacs exists"
 else
-    yay -S emacs
+    yay texinfo tree-sitter gnutls
+    git clone --branch emacs-29 https://git.savannah.gnu.org/git/emacs.git ~/fun/emacs
+    cd ~/fun/emacs
+    ./autogen.sh
+    ./configure --without-x --without-ns --without-native-compilation --with-tree-sitter
+    make
+    sudo make install
 fi
